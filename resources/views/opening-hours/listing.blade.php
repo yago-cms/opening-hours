6ac27e022b987632fab4288036bfaf2d9bd62a5f
Johan <pythas@gmail.com>
2022-04-25 13:35:26 +0000

    Fix imports

--- resources/views/opening-hours/listing.blade.php
+++ resources/views/opening-hours/listing.blade.php
@@ -27,46 +27,48 @@
         </li>
     @endforeach
 
-    <div class="opening-hours-today__exceptions">
-        <h4 class="opening-hours-today__exceptions__heading">
-            {{ __('yago-opening-hours::opening-hours.exceptional_opening_hours') }}
-        </h4>
+    @if (count($openingHourExceptions) > 0)
+        <div class="opening-hours-today__exceptions">
+            <h4 class="opening-hours-today__exceptions__heading">
+                {{ __('yago-opening-hours::opening-hours.exceptional_opening_hours') }}
+            </h4>
 
-        <ul class="opening-hours-today__exceptions__list">
-            @foreach ($openingHourExceptions as $openingHourException)
-                @php
-                    $times = collect([$openingHourException->open, $openingHourException->close]);
-                    $times = $times->map(fn($time) => Str::replaceLast(':00', '', $time));
-                @endphp
+            <ul class="opening-hours-today__exceptions__list">
+                @foreach ($openingHourExceptions as $openingHourException)
+                    @php
+                        $times = collect([$openingHourException->open, $openingHourException->close]);
+                        $times = $times->map(fn($time) => Str::replaceLast(':00', '', $time));
+                    @endphp
 
-                <li class="opening-hours-today__exceptions__list__item">
-                    {{ $openingHourException->name }}
+                    <li class="opening-hours-today__exceptions__list__item">
+                        {{ $openingHourException->name }}
 
-                    @if (date('Y-m-d') == $openingHourException->date)
-                        ({{ __('yago-opening-hours::opening-hours.today') }})
-                        :
-                    @else
-                        ({{ date('l jS', strtotime($openingHourException->date)) }})
-                        :
-                    @endif
-
-                    @if ($openingHourException->is_closed)
-                        {{ __('yago-opening-hours::opening-hours.closed') }}
-                    @else
-                        {{ $times->join('-') }}
-
-                        @if ($openingHourException->is_closed_for_lunch)
-                            @php
-                                $times = collect([$openingHourException->lunch_close, $openingHourException->lunch_open]);
-                                $times = $times->map(fn($time) => Str::replaceLast(':00', '', $time));
-                            @endphp
-
-                            ({{ __('yago-opening-hours::opening-hours.closed_for_lunch') }}:
-                            {{ $times->join('-') }})
+                        @if (date('Y-m-d') == $openingHourException->date)
+                            ({{ __('yago-opening-hours::opening-hours.today') }})
+                            :
+                        @else
+                            ({{ date('l jS', strtotime($openingHourException->date)) }})
+                            :
                         @endif
-                    @endif
-                </li>
-            @endforeach
-        </ul>
-    </div>
+
+                        @if ($openingHourException->is_closed)
+                            {{ __('yago-opening-hours::opening-hours.closed') }}
+                        @else
+                            {{ $times->join('-') }}
+
+                            @if ($openingHourException->is_closed_for_lunch)
+                                @php
+                                    $times = collect([$openingHourException->lunch_close, $openingHourException->lunch_open]);
+                                    $times = $times->map(fn($time) => Str::replaceLast(':00', '', $time));
+                                @endphp
+
+                                ({{ __('yago-opening-hours::opening-hours.closed_for_lunch') }}:
+                                {{ $times->join('-') }})
+                            @endif
+                        @endif
+                    </li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
 </div>
